6b110032351e12c7dec79cdc3e734fb44452f8a3
Marcus Soll <git@msoll.eu>
2021-07-23 18:24:56 +0200

    Show changed favicon on login or new topics/posts

--- update_icon.sh
+++ update_icon.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 convert -size 256x256 -resize 256x256 -extent 256x256 -gravity center -fuzz 30% -transparent white static/Logo.svg static/favicon.ico
+convert -size 256x256 -resize 256x256 -extent 256x256 -gravity center -fuzz 30% -transparent white static/Star.svg static/faviconStar.ico
